04ffbd025abbf328b98a676a5793aef50bad4ed2
Nate Brake <njbrake@gmail.com>
2018-12-11 21:13:50 -0500

    added docs

---
+++ docs/precache-manifest.6197d2457012e10715f98f9e732821b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c181be9ee3b602961ca4d67c560054f",
+    "url": "/static/media/landingVid.0c181be9.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ed8d4561e98fbc0c6ac",
+    "url": "/static/js/main.1ed8d456.chunk.js"
+  },
+  {
+    "revision": "93ad35e08c9977aaa777",
+    "url": "/static/js/1.93ad35e0.chunk.js"
+  },
+  {
+    "revision": "1ed8d4561e98fbc0c6ac",
+    "url": "/static/css/main.c6df60c0.chunk.css"
+  },
+  {
+    "revision": "8ecb05673e7f80040ed2752335abb3f3",
+    "url": "/index.html"
+  }
+];
